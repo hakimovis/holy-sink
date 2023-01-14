9cca18f3f8afec4d67d89b1ef33892e500e7aee7
i.hakimov <i.hakimov@fun-box.ru>
2023-01-14 15:28:56 +0500

    styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    {notice: "alert-info", success: "alert-success", error: "alert-danger", alert: "alert-danger"}.
+      fetch(flash_type.to_sym, "alert-#{flash_type}")
+  end
+
+  def flash_messages
+    output_html = "".html_safe
+
+    flash.each do |flash_type, message|
+      css_class = bootstrap_class_for(flash_type) || flash_type.to_s
+      output_html += content_tag(:div, message, class: "alert #{css_class}", role: "alert") do
+        message
+      end
+    end
+
+    output_html
+  end
 end
