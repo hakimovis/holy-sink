5eb886c779ae95fcb42a3e3aa56ce9f4bbfc7d96
Михаил Павлович <darthmuxa@gmail.com>
2024-06-21 23:21:05 +0500

    Add the administrator role to the first user in the group. Allow the administrator to remove group members.

---
+++ app/controllers/administration_controller.rb
@@ -0,0 +1,24 @@
+class AdministrationController < ApplicationController
+  helper_method :team_users
+  before_action :require_team!
+  before_action :require_user!
+  def index
+  end
+
+  def destroy
+    @user_name = User.find(params[:id]).name
+    User.destroy(params[:id])
+    redirect_with_message("Пользователь #{@user_name} удален успешно")
+  end
+
+  def team_users
+    User.where(team_name: current_team[:name])
+  end
+
+  private
+
+  def redirect_with_message(string)
+    session[:return_to] ||= request.referer
+    redirect_to session.delete(:return_to), notice: string
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -8,6 +9,7 @@ Rails.application.routes.draw do
   resources :team_login, only: [:index, :create,] do
     delete :index, on: :collection, action: :log_out
   end
+  resources :administration, only: [:index, :destroy]
   resources :user_login
   resources :my_days do
     post :comments, action: 'update_comment', on: :collection
